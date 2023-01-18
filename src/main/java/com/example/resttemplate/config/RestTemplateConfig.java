d0af502765836c412550ed660b4bcbc8380bb086
SeHoonBae <bey1548@naver.com>
2023-01-18 13:09:41 +0900

    https 통신 추가

--- src/main/java/com/example/resttemplate/config/RestTemplateConfig.java
+++ src/main/java/com/example/resttemplate/config/RestTemplateConfig.java
@@ -3,6 +3,12 @@ package com.example.resttemplate.config;
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactoryBuilder;
+import org.apache.hc.client5.http.ssl.TrustAllStrategy;
+import org.apache.hc.core5.ssl.SSLContexts;
 import org.apache.hc.core5.util.TimeValue;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,21 +16,36 @@ import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.http.HttpClient;
+import javax.net.ssl.SSLContext;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class RestTemplateConfig {
 
-    private ClientHttpRequestFactory getClientHttpRequestFactory(){
+    private ClientHttpRequestFactory getClientHttpRequestFactory() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
         int timeout = 5000;
         RequestConfig config = RequestConfig.custom()
                 .setConnectionRequestTimeout(timeout, TimeUnit.MILLISECONDS)
                 .setConnectionKeepAlive(TimeValue.of(timeout, TimeUnit.MILLISECONDS))
                 .build();
 
+        SSLContext sslContext = SSLContexts.custom()
+                .loadTrustMaterial(null, new TrustAllStrategy()).build();
+
+        SSLConnectionSocketFactory sslConnectionSocketFactory = SSLConnectionSocketFactoryBuilder.create()
+                .setSslContext(sslContext)
+                .build();
+
+        HttpClientConnectionManager cm = PoolingHttpClientConnectionManagerBuilder.create()
+                .setSSLSocketFactory(sslConnectionSocketFactory)
+                .build();
+
         CloseableHttpClient client = HttpClientBuilder
                 .create()
+                .setConnectionManager(cm)
                 .setDefaultRequestConfig(config)
                 .build();
 
@@ -32,7 +53,7 @@ public class RestTemplateConfig {
     }
 
     @Bean
-    public RestTemplate restTemplate(){
+    public RestTemplate restTemplate() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
         RestTemplate restTemplate = new RestTemplate(getClientHttpRequestFactory());
         return restTemplate;
     }
